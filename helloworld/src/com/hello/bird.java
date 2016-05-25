9bee2522a42378aa4317b42cfab13c62a48cb200
xujiaqi <xujiaqi@bestpay.com.cn>
2016-05-25 10:51:10 +0800

    first java

---
+++ helloworld/src/com/hello/bird.java
@@ -0,0 +1,11 @@
+package com.hello;
+
+/**
+ * Created by jerry on 2016/5/25 0025.
+ */
+public class bird {
+    public static void main (String args[])
+    {
+        System.out.println("im a bird");
+    }
+}
